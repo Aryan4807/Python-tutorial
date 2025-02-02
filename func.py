eb19112d8378e3595e35e7b089c3f6e1a7a2c013
Aryan4807 <65225779+Aryan4807@users.noreply.github.com>
2025-02-02 17:40:30 +0530

    changes for func recur

---
+++ func.py
@@ -0,0 +1,13 @@
+# def sum(n):
+#     if(n==0):
+#         return n
+#     s=n+sum(n-1)
+#     return s
+
+# print(sum(5))
+
+def inch_cm(n):
+    return n*2.54
+# int n;
+n=input("enter a num",)
+print(inch_cm(int(n)))
